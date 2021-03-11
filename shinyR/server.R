384bb24cfcc9794f8282ab1904943d639885b043
Labibsm <31006280+labiblm@users.noreply.github.com>
2021-03-11 12:58:04 +0000

    remove shiny

--- shinyR/server.R
+++
@@ -1,9 +0,0 @@
-server <- function(input, output){
-  output$map <- renderLeaflet({
-    leaflet() %>% 
-      addRasterImage(r, layerId = "values") %>% 
-      addLegend(pal = pal, values = values(r)
-                addMouseCoordinates() %>%
-                  addImageQuery(r, type="mousemove", layerId = "values")
-  })
-}
